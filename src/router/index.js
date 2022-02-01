4c01865f6c1651e6f48a5fc375cce054fe8197bb
natan1563 <natromafe@gmail.com>
2022-02-01 19:37:03 -0300

    Fix: Set Chat Id Route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SelectedChat from '@/components/SelectedChat.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/chat/:id',
+    name: 'SelectedChat',
+    component: SelectedChat
+  }
 ]
 
 const router = new VueRouter({
